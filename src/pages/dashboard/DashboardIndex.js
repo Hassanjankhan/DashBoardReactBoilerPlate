3171623adb5ce2ca9bb093315742f785a0136426
Hassanjankhan <hassanjankhan729@gmail.com>
2023-02-17 00:18:47 +0500

    dashboard boilerplate

---
+++ src/pages/dashboard/DashboardIndex.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DashboardIndex = (prop) => {
+    return (
+        <div>DashboardIndex</div>
+    );
+};
+
+export default DashboardIndex;
---
+++ src/pages/installation/InstallationPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InstallationPage = (props) => {
+    return (
+        <div>InstallationPage</div>
+    );
+};
+
+export default InstallationPage;
